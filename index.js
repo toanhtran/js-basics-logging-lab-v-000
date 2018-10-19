811a6525547eb39982d687a2840bd22d875a268d
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-19 20:49:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ const driver = 'bob';
 
 console.log('this code was called');
 
-console.log(`${driver}`);
+console.log(driver);
 
-console.log(function driver(driver, str){
-  let str = 'is the driver variable name';
-  return (`${driver} + str`);
-})
